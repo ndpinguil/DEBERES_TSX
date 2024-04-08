fdc8040041b656c9213035bed37dba4eec911344
ndpinguil <116099207+ndpinguil@users.noreply.github.com>
2024-04-08 08:28:54 -0500

    second commit

--- exercism_deber/src/App.tsx
+++ exercism_deber/src/App.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+//Ejercicio #1
+/*import React from 'react';
 import TwoFer from './TwoFer';
 import './App.css'
 
@@ -13,4 +14,60 @@ const App: React.FC = () => {
   );
 }
 
-export default App;
+export default App;*/
+
+// Ejercicio #2
+
+import './App.css'
+import React from 'react';
+
+const colorCode = (color: string): number => {
+  const colorMap: { [color: string]: number } = {
+    'black': 0,
+    'brown': 1,
+    'red': 2,
+    'orange': 3,
+    'yellow': 4,
+    'green': 5,
+    'blue': 6,
+    'violet': 7,
+    'grey': 8,
+    'white': 9,
+  };
+
+  if (colorMap[color] !== undefined) {
+    return colorMap[color];
+  } else {
+    throw new Error('Invalid color');
+  }
+};
+
+const COLORS: string[] = [
+  'black',
+  'brown',
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'violet',
+  'grey',
+  'white',
+];
+
+const ColorCodeComponent: React.FC = () => {
+  return (
+    <div>
+      <h1>Color Codes</h1>
+      <ul>
+        {COLORS.map((color, index) => (
+          <li key={index}>
+            {color}: {colorCode(color)}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ColorCodeComponent;
